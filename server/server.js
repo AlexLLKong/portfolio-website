2146fd8027e1fd9ef14417e346039dbe6731a2f4
AlexLLKong <alexkonghockey@gmail.com>
2020-10-26 16:08:23 -0700

    Main section basic layout done

--- server/server.js
+++ server/server.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const path = require('path')
+const connectLivereload = require("connect-livereload");
 const app = express();
+const livereload = require('livereload')
 
+const liveReloadServer = livereload.createServer()
+liveReloadServer.watch(path.join(__dirname, 'public'))
+
+app.use(connectLivereload());
 app.use(express.static(path.join(__dirname, 'public')))
+
+liveReloadServer.server.once("connection", () => {
+	setTimeout(() => {
+		liveReloadServer.refresh("/");
+	}, 100);
+});
 
 const PORT = process.env.PORT || 5000;
 
