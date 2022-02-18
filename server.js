fce5c4f261cf29714e0cd7b1ea245a3f77e05c86
Alex Kong <alexkonghockey@gmail.com>
2022-02-18 15:47:09 -0800

    add production serve

--- server.js
+++ server.js
@@ -3,20 +3,27 @@ const path = require('path')
 const connectLivereload = require("connect-livereload");
 const app = express();
 const livereload = require('livereload')
-
-const liveReloadServer = livereload.createServer()
-liveReloadServer.watch(path.join(__dirname, 'public'))
-
-app.use(connectLivereload());
-app.use(express.static(path.join(__dirname, 'public')))
-
-liveReloadServer.server.once("connection", () => {
-	setTimeout(() => {
-		liveReloadServer.refresh("/");
-	}, 100);
-});
-
+const fs = require('fs');
 const PORT = process.env.PORT || 5000;
+if (process.env.PORT) {
+  app.get("/", (req, res) => {
+    res.send(fs.readFileSync("./public/index.html", "utf8"));
+  })
+}
+else {
+  const liveReloadServer = livereload.createServer()
+  liveReloadServer.watch(path.join(__dirname, 'public'))
 
-app.listen(PORT, () => {console.log(`Server running on port: ${PORT}`)})
+  app.use(connectLivereload());
+  app.use(express.static(path.join(__dirname, 'public')))
+
+  liveReloadServer.server.once("connection", () => {
+    setTimeout(() => {
+      liveReloadServer.refresh("/");
+    }, 100);
+  });
+}
+
+
+app.listen(PORT, () => { console.log(`Server running on port: ${PORT}`) })
 
